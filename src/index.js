dd1f1a6de85028ee388641bd1a519bcfb1d3e69c
ju8ko <julia.x.kobzar@gmail.com>
2022-10-11 18:09:20 +0200

    The exerise is done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
+import React from "react";
+import { createRoot } from "react-dom/client";
+//we use ReactDOM to render into the DOM
 
 // WHAT: This variable returns contains the html to render
+const name = "James";
+const age = "12";
 let output = (
-	<span>
-		James is 12 years old
-	</span>
+  <span>
+    {name} is {age}years old
+  </span>
 );
 
 // WHERE: A DOM element that will contain the entire react generated html
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
-// create a root
-const root = ReactDOM.createRoot(container);
+//create a root
+const root = createRoot(container);
 
 //render app to root
 root.render(output);
-
-
-
-
